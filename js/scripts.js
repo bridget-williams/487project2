41857f87c2bd2b9f1c3dad70043a3dff14dde22d
Bridget Williams <bridget_williams1@unc.edu>
2018-10-13 19:58:28 -0400

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    console.log('Test');
+
+
+
+    $.backstretch("img/citymap.jpg");
+
+    $('.introtext').slick({
+    });
+
+
+
+
+
+//Close Document Ready Function
+});
