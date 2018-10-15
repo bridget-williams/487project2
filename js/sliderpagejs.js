eb9c4e6af6a68df5c5d85a3c7315bb83a36bac0f
Bridget Williams <bridget_williams1@unc.edu>
2018-10-14 22:27:04 -0400

    Complete JS function for coloring svg elements as user clicks through slides

--- js/sliderpagejs.js
+++ js/sliderpagejs.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     console.log('Test');
 
 
@@ -13,30 +13,87 @@ $(function(){
     }
 
 
-
-$('#people').click(function(){
-    $(this).css('filter', 'none');
-})
-
+    $('#people').click(function () {
+        $(this).css('filter', 'none');
+    })
 
 
 
-    i++;
-    console.log(i);
+    // $('.slick-next').click(function(e){
+    //     console.log(e.target);
+    //     $('#people').css('filter', 'none');
+    // })
 
     var i = 0;
-$('.slick-next').click(function(){
-    console.log('next clicked');
-    i++;
     console.log(i);
-    if (i==1) {
-        $('#people').css('filter', 'none');
-    }
-})
+    $('body').on('click', '.slick-next', () => {
+        console.log('next clicked');
+        i++;
+        console.log(i);
+        if (i == 1) {
+            $('#roads').css('filter', 'none');
+        } else if (i==2){
+            $('#energy').css('filter', 'drop-shadow(16px 16px 20px yellow)');
+        }else if (i==3) {
+            $('#waste').css('filter', 'none');
+        }else if (i==4) {
+            $('#trees').css('filter', 'none');
+        }else if (i==5) {
+            $('#transit').css('filter', 'drop-shadow(16px 16px 20px yellow)');
+        }else if (i==6) {
+            $('#lighting').css('filter', 'none');
+        }else if (i==7) {
+            $('#seating').css('filter', 'none');
+        }else if (i==8) {
+            $('#building').css('filter', 'none');
+        }else if (i==9) {
+            $('#grass').css('filter', 'none');
+        }else if (i==10) {
+            $('#people').css('filter', 'none');
+        }else  {
+            i==0;
+        }
 
+    });
 
+    $('body').on('click', '.slick-prev', () => {
+        console.log('back clicked');
+        i--;
+        console.log(i);
+        if (i == 0) {
+            $('#roads').css('filter', 'grayscale(100%)');
+        } else if (i==1){
+            $('#energy').css('filter', 'grayscale(100%)');
+        }else if (i==2) {
+            $('#waste').css('filter', 'grayscale(100%)');
+        }else if (i==3) {
+            $('#trees').css('filter', 'grayscale(100%)');
+        }else if (i==4) {
+            $('#transit').css('filter', 'grayscale(100%)');
+        }else if (i==5) {
+            $('#lighting').css('filter', 'grayscale(100%)');
+        }else if (i==6) {
+            $('#seating').css('filter', 'grayscale(100%)');
+        }else if (i==7) {
+            $('#building').css('filter', 'grayscale(100%)');
+        }else if (i==8) {
+            $('#grass').css('filter', 'grayscale(100%)');
+        }else if (i==9) {
+            $('#people').css('filter', 'grayscale(100%)');
+        }else  {
+            i==0;
+        }
 
-
+    });
+    
+    $('.slick-next').click(function () {
+        console.log('next clicked');
+        i++;
+        console.log(i);
+        if (i == 1) {
+            $('#people').css('filter', 'none');
+        }
+    })
 
 
     $('.slidertext').slick({
@@ -44,7 +101,6 @@ $('.slick-next').click(function(){
     });
 
 
-
     //close ready function
-    });
+});
 
